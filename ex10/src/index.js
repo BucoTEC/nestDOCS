fc852f7351ac25eb606282e26df5f8a71321332a
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2021-09-16 21:56:55 +0200

    work done

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+  // Only change code below this line
+  var myFirstName = "Adnan";
+  var myLastName = "Bucalovic";
+  var myHomeTown = "Sarajevo";
+  // Only change code above this line
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+module.exports = main;
