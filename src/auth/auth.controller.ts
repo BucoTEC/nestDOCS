7667048b62fdd25a1138cfcf4f306900b67a0f1f
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-24 23:46:44 +0200

    add basic auth dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthControler {
   constructor(private authService: AuthService) {}
   @Post('login')
-  login() {
+  login(@Body() dto: AuthDto) {
+    console.log(dto);
+
     return this.authService.login();
   }
 
